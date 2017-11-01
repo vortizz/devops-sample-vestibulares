13370c9f64065904b6d414f3bbe4385b8cd0ff32
Tomaz <tmsski@yahoo.com.br>
2017-11-01 15:13:04 -0200

    cadastro de vestibulares

--- app/views.py
+++ app/views.py
@@ -5,7 +5,7 @@ Definition of views.
 from django.shortcuts import render
 from django.http import HttpRequest
 from django.template import RequestContext
-from app.models import Curso
+from app.models import Curso, Vestibular
 from datetime import datetime
 
 def home(request):
@@ -59,6 +59,19 @@ def cadastro_cursos(request):
             'title':'Cadastro de cursos',
 #            'cursos': ['ADS' , 'SI', 'CC'],
             'cursos': Curso.objects.all(),
+            'year':datetime.now().year,
+        })
+    )
+
+def cadastro_vestibulares(request):
+    assert isinstance(request, HttpRequest)
+    return render(
+        request,
+        'app/cadastro_vestibulares.html',
+        context_instance = RequestContext(request,
+        {
+            'title':'Cadastro de vestibulares',
+            'vestibulares': Vestibular.objects.all(),
             'year':datetime.now().year,
         })
     )
